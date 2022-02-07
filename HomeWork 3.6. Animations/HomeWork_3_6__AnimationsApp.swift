30d9605e252db87deb4776e497d7acdbe3a93fa9
Irina Bex <92269031+Irchhhh@users.noreply.github.com>
2022-02-07 20:35:39 +0300

    Initial Commit

---
+++ HomeWork 3.6. Animations/HomeWork_3_6__AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork_3_6__AnimationsApp.swift
+//  HomeWork 3.6. Animations
+//
+//  Created by Ирина on 07.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork_3_6__AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
