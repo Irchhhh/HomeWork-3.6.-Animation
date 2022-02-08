85e477536296b7efbd5f6eb09e6196a525f90afa
Irina Bex <92269031+Irchhhh@users.noreply.github.com>
2022-02-08 14:47:56 +0300

    first commit

---
+++ HomeWork 3.6. Animations/DropRainView.swift
@@ -0,0 +1,20 @@
+//
+//  DropRainView.swift
+//  HomeWork 3.6. Animations
+//
+//  Created by Ирина on 07.02.2022.
+//
+
+import SwiftUI
+
+struct DropRainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DropRainView_Previews: PreviewProvider {
+    static var previews: some View {
+        DropRainView()
+    }
+}
---
+++ HomeWork 3.6. Animations/WavesView.swift
@@ -0,0 +1,20 @@
+//
+//  WavesView.swift
+//  HomeWork 3.6. Animations
+//
+//  Created by Ирина on 07.02.2022.
+//
+
+import SwiftUI
+
+struct WavesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WavesView_Previews: PreviewProvider {
+    static var previews: some View {
+        WavesView()
+    }
+}
